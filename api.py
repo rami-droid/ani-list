e453b0999a41ded087d3998d80a18090e951ffd7
rami <billdoors90@gmail.com>
2025-09-30 11:27:26 +0200

    add status code error handling

--- api.py
+++ api.py
@@ -8,25 +8,43 @@ def get_airing(nsfw=False):
     response = requests.get(
         base_url + url_top, params={"filter": "airing", "sfw": str(not nsfw).lower()}
     )
-    data = response.json()
-    return data
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    elif response.status_code == 400:
+        return "error: 400"
 
 
 def get_top(nsfw=False):
     response = requests.get(
         base_url + "/top/anime", params={"sfw": str(not nsfw).lower()}
     )
-    data = response.json()
-    return data
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    elif response.status_code == 400:
+        return "error: 400"
 
 
 def search_anime(query):
     response = requests.get(base_url + "/anime", params={"q": query})
-    data = response.json()
-    return data
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    elif response.status_code == 400:
+        return "error: 400"
 
 
 def search_anime_full(id):
     response = requests.get(base_url + "/anime/" + str(id) + "/full")
-    data = response.json()
-    return data
+
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    elif response.status_code == 400:
+        return "error: 400"
+
+
+def get_genres():
+    # response = requests.get(base_url + )
+    pass

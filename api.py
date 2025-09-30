96145373e0e3b2777b4bca0d415201ee66c55769
rami <billdoors90@gmail.com>
2025-09-30 11:41:48 +0200

    improve api err handling

--- api.py
+++ api.py
@@ -1,18 +1,27 @@
+import re
 import requests
 
 base_url = "https://api.jikan.moe/v4"
 url_top = "/top/anime"
 
 
+class ApiError(Exception):
+    def __init__(self, status, message):
+        self.status = status
+        self.message = message
+        pass
+        super().__init__(f"API Error {status}: {message}")
+
+
 def get_airing(nsfw=False):
     response = requests.get(
         base_url + url_top, params={"filter": "airing", "sfw": str(not nsfw).lower()}
     )
-    if response.status_code == 200:
-        data = response.json()
-        return data
-    elif response.status_code == 400:
-        return "error: 400"
+    if response.status_code != 200:
+        raise ApiError(response.status_code, response.reason)
+
+    data = response.json()
+    return data
 
 
 def get_top(nsfw=False):
